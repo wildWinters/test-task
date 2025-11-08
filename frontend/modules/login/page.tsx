65fbdf1e534f30b3bf6620adfe3e4331200a00a6
@marc.lyubarov <pavlo.kramar17@gmail.com>
2025-11-08 16:44:54 +0200

    refactor:delete folder frontend

--- frontend/modules/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { RegistrationCardWrapper } from "./registration-card/registration-card-wrapper"
-export default function Login() {
-
-  return (
-    <div className="flex items-center justify-center min-w-[100vw] min-h-screen">
-      <RegistrationCardWrapper  />
-    </div>
-  )
-}
